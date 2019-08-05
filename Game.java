e8abf7d69aed215e3ee149f96cf1392e8a523f87
vuwestin <36426709+vuwestin@users.noreply.github.com>
2019-08-04 23:17:56 -0700

    Add files via upload

--- Game.java
+++ Game.java
@@ -1,4 +1,4 @@
-package yee;
+package sweep;
 import javax.swing.*;
 
 import java.awt.*;
@@ -13,25 +13,20 @@ public class Game extends JPanel implements ActionListener, MouseListener {
 	private int correctFlagCounter_;
 	private int disabledCellCounter_;
 	/*
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
-	 * 
+
 	 * */
-	private ImageIcon one = new ImageIcon("C:\\Users\\Westin\\Desktop\\Minesweeper\\Images\\one.png");
-	private ImageIcon two = new ImageIcon("C:\\Users\\Westin\\Desktop\\Minesweeper\\Images\\two.png");
-	private ImageIcon three = new ImageIcon("C:\\Users\\Westin\\Desktop\\Minesweeper\\Images\\three.png");
-	private ImageIcon four = new ImageIcon("C:\\Users\\Westin\\Desktop\\Minesweeper\\Images\\four.png");
-	private ImageIcon five = new ImageIcon("C:\\Users\\Westin\\Desktop\\Minesweeper\\Images\\five.png"); //all icons needed for the game
-	private ImageIcon six = new ImageIcon("C:\\Users\\Westin\\Desktop\\Minesweeper\\Images\\six.png");
-	private ImageIcon seven = new ImageIcon("C:\\Users\\Westin\\Desktop\\Minesweeper\\Images\\seven.png");
-	private ImageIcon eight = new ImageIcon("C:\\Users\\Westin\\Desktop\\Minesweeper\\Images\\eight.png");
-	private ImageIcon empty = new ImageIcon("C:\\Users\\Westin\\Desktop\\Minesweeper\\Images\\empty.png");
-	private ImageIcon unpressed = new ImageIcon("C:\\Users\\Westin\\Desktop\\Minesweeper\\Images\\unpressed.png");
-	private ImageIcon flag = new ImageIcon("C:\\Users\\Westin\\Desktop\\Minesweeper\\Images\\flag.png");
-	private ImageIcon mine = new ImageIcon("C:\\Users\\Westin\\Desktop\\Minesweeper\\Images\\mine.png");
+	private ImageIcon one = new ImageIcon("D:\\wee\\Desktop\\Minesweeper\\Images\\one.png");
+	private ImageIcon two = new ImageIcon("D:\\wee\\Desktop\\Minesweeper\\Images\\two.png");
+	private ImageIcon three = new ImageIcon("D:\\wee\\Desktop\\Minesweeper\\Images\\three.png");
+	private ImageIcon four = new ImageIcon("D:\\wee\\Desktop\\Minesweeper\\Images\\four.png");
+	private ImageIcon five = new ImageIcon("D:\\wee\\Desktop\\Minesweeper\\Images\\five.png"); //all icons needed for the game
+	private ImageIcon six = new ImageIcon("D:\\wee\\Desktop\\Minesweeper\\Images\\six.png");
+	private ImageIcon seven = new ImageIcon("D:\\wee\\Desktop\\Minesweeper\\Images\\seven.png");
+	private ImageIcon eight = new ImageIcon("D:\\wee\\Desktop\\Minesweeper\\Images\\eight.png");
+	private ImageIcon empty = new ImageIcon("D:\\wee\\Desktop\\Minesweeper\\Images\\empty.png");
+	private ImageIcon unpressed = new ImageIcon("D:\\wee\\Desktop\\Minesweeper\\Images\\unpressed.png");
+	private ImageIcon flag = new ImageIcon("D:\\wee\\Desktop\\Minesweeper\\Images\\flag.png");
+	private ImageIcon mine = new ImageIcon("D:\\wee\\Desktop\\Minesweeper\\Images\\mine.png");
 	
 	private Image scaledFlag = flag.getImage().getScaledInstance(55, 55,java.awt.Image.SCALE_SMOOTH);
 	private Image scaledUnpressed =unpressed.getImage().getScaledInstance(55, 55,java.awt.Image.SCALE_SMOOTH);
@@ -48,7 +43,7 @@ public class Game extends JPanel implements ActionListener, MouseListener {
 	
 	private ImageIcon[][] sol_; //solution for the game
 	//private JButton[][] cellArr_;
-	private Cell[][] cellArr_;
+	private Cell[][] cellArr_; //2d array of cells that the player sees
 	private JPanel[][] panelArr_;
 	/**
 	 * @param rows
@@ -75,10 +70,11 @@ public class Game extends JPanel implements ActionListener, MouseListener {
 	empty = new ImageIcon(scaledEmpty);
 	mine = new ImageIcon(scaledMine);
 	
-	sol_ = new ImageIcon[rows_][cols_]; //solution grid
-	for(int i = 0; i < rows_; i++)
+	sol_ = new ImageIcon[rows_+2][cols_+2]; //solution grid. the plus 2 is to have a border of mines around the actual game
+	//this saves time from having to check all of the borders and corners individually for outOfBounds exceptions
+	for(int i = 0; i < rows_+2; i++)
 	{
-		for(int j = 0; j < cols_; j++)
+		for(int j = 0; j < cols_+2; j++)
 		{
 			sol_[i][j] = empty; //sets all to blank to start
 		}
@@ -93,170 +89,20 @@ public class Game extends JPanel implements ActionListener, MouseListener {
 			randomX = (int)(Math.random() * rows_); //checks if mine is already there
 			randomY = (int)(Math.random() * cols_);
 		}
-		sol_[randomX][randomY] = mine; //puts mine in random spot
+		sol_[randomX][randomY] = mine; //puts mine in random spot. adds one to account for the border of empty mines
+		
 	}
 	
-	for(int i = 0; i < rows_; i++) //checks every panel for mines around it and sets image accordingly
+	
+	
+	for(int i = 1; i < rows_+1; i++) //checks every panel for mines around it and sets image accordingly, while accounting for border
 	{
-		for(int j = 0; j< cols_; j++)
+		for(int j = 1; j< cols_+1; j++) //starts at 1 end ends at cols_ + 1 to account for the border
 		{
 			if(sol_[i][j] != mine)
 			{
 				int mineCounter = 0;
-				if(i == 0 && j == 0) //checks for top left corner
-				{
-					if(sol_[i+1][j] == mine)
-					{
-						mineCounter++;
-					}
-					if(sol_[i+1][j+1] == mine)
-					{
-						mineCounter++;
-					}
-					if(sol_[i][j+1] == mine)
-					{
-						mineCounter++;
-					}
-				}
-				else if(i == 0 && j == cols_ -1) //checks for top right corner
-				{
-					if(sol_[i+1][j] == mine)
-					{
-						mineCounter++;
-					}
-					if(sol_[i+1][j-1] == mine)
-					{
-						mineCounter++;
-					}
-					if(sol_[i][j-1] == mine)
-					{
-						mineCounter++;
-					}
-				}
-				else if(i == rows_-1 && j == 0) //checks for bottom left corner
-				{
-					if(sol_[i-1][j] == mine)
-					{
-						mineCounter++;
-					}
-					if(sol_[i-1][j+1] == mine)
-					{
-						mineCounter++;
-					}
-					if(sol_[i][j+1] == mine)
-					{
-						mineCounter++;
-					}
-				}
-				else if(i == rows_-1 && j == cols_-1) //checks for bottom right corner
-				{
-					if(sol_[i-1][j] == mine)
-					{
-						mineCounter++;
-					}
-					if(sol_[i-1][j-1] == mine)
-					{
-						mineCounter++;
-					}
-					if(sol_[i][j-1] == mine)
-					{
-						mineCounter++;
-					}
-				}
-				else if(i == 0) //case for top row
-				{
-					if(sol_[i][j-1] == mine)
-					{
-						mineCounter++;
-					}
-					if(sol_[i+1][j-1] == mine)
-					{
-						mineCounter++;
-					}
-					if(sol_[i+1][j] == mine)
-					{
-						mineCounter++;
-					}
-					if(sol_[i+1][j+1] == mine)
-					{
-						mineCounter++;
-					}
-					if(sol_[i][j+1] == mine)
-					{
-						mineCounter++;
-					}
-				}
-				else if(i == rows_-1) //case for bottom row
-				{
-					if(sol_[i][j-1] == mine)
-					{
-						mineCounter++;
-					}
-					if(sol_[i-1][j-1] == mine)
-					{
-						mineCounter++;
-					}
-					if(sol_[i-1][j] == mine)
-					{
-						mineCounter++;
-					}
-					if(sol_[i-1][j+1] == mine)
-					{
-						mineCounter++;
-					}
-					if(sol_[i][j+1] == mine)
-					{
-						mineCounter++;
-					}
-				}
-				else if(j == 0) //case for leftmost column
-				{
-					if(sol_[i-1][j] == mine)
-					{
-						mineCounter++;
-					}
-					if(sol_[i-1][j+1] == mine)
-					{
-						mineCounter++;
-					}
-					if(sol_[i][j+1] == mine)
-					{
-						mineCounter++;
-					}
-					if(sol_[i+1][j+1] == mine)
-					{
-						mineCounter++;
-					}
-					if(sol_[i+1][j] == mine)
-					{
-						mineCounter++;
-					}
-				}
-				else if(j == cols_ -1) //case for rightmost side
-				{
-					if(sol_[i-1][j] == mine)
-					{
-						mineCounter++;
-					}
-					if(sol_[i-1][j-1] == mine)
-					{
-						mineCounter++;
-					}
-					if(sol_[i][j-1] == mine)
-					{
-						mineCounter++;
-					}
-					if(sol_[i+1][j-1] == mine)
-					{
-						mineCounter++;
-					}
-					if(sol_[i+1][j] == mine)
-					{
-						mineCounter++;
-					}
-				}
-				else //other cases where we don't have to worry about bounds issues
-				{
+
 					if(sol_[i-1][j] == mine)
 					{
 						mineCounter++;
@@ -289,7 +135,7 @@ public class Game extends JPanel implements ActionListener, MouseListener {
 					{
 						mineCounter++;
 					}
-				}
+//				}
 				
 				if(mineCounter == 0)
 				{
@@ -309,7 +155,7 @@ public class Game extends JPanel implements ActionListener, MouseListener {
 				}
 				else if(mineCounter == 4) //sets the actual label corresponding to number of mines around it
 				{							//ex if 5 mines are around this cell it will be labeled with a 5
-					sol_[i][j] = four;
+					sol_[i][j] = four;		//use switch case here later
 				}
 				else if(mineCounter == 5)
 				{
@@ -332,24 +178,32 @@ public class Game extends JPanel implements ActionListener, MouseListener {
 	}
 	
 	setLayout(new GridLayout(rows_, cols_, 0,0)); //creates layout of the panel
-	cellArr_ = new Cell[rows_][cols_];
+	cellArr_ = new Cell[rows_+2][cols_+2]; //+2 because of the empty cell border
 	panelArr_ = new JPanel[rows_][cols_]; //sets 2d array to whatever size the user wants
+	for(int i = 0; i < (rows_ + 2); i++)
+	{
+		for(int j = 0; j < (cols_ + 2);j++)
+		{
+			cellArr_[i][j] = new Cell(sol_[i][j]);  //sets cells disabledIcon to the solution
+			cellArr_[i][j].x_ = i;
+			cellArr_[i][j].y_ = j; //labels the Cells x and y values for use later
+			cellArr_[i][j].setIcon(unpressed);
+			cellArr_[i][j].addActionListener(this); //adds action listener to later click on it and reveal value
+			cellArr_[i][j].addMouseListener(this); //adds mouse listener for the flags
+			//try putting this in the for loop after this later since the border doesnt need action listeners
+		}
+	}
 	for(int i = 0; i < rows_; i++) //for however many cells there are create a panel and add a Cell to it.
 	{								//then add that panel to the Game Panel
 		for(int j =0; j < cols_; j++)
 		{
 			panelArr_[i][j] = new JPanel();
 			panelArr_[i][j].setLayout(new BorderLayout());
-			cellArr_[i][j] = new Cell(sol_[i][j]);  //sets cells disabledIcon to the solution
-			cellArr_[i][j].x_ = i;
-			cellArr_[i][j].y_ = j; //labels the Cells x and y values for use later
-			cellArr_[i][j].setIcon(unpressed);
+
 			panelArr_[i][j].add(cellArr_[i][j], BorderLayout.CENTER);
 			panelArr_[i][j].validate(); //needed to make sure the picture is right
 			panelArr_[i][j].revalidate();
 
-			cellArr_[i][j].addActionListener(this); //adds action listener to later click on it and reveal value
-			cellArr_[i][j].addMouseListener(this); //adds mouse listener for the flags
 			add(panelArr_[i][j]); //adds all of this panel to the actual game
 			validate(); //makes sure it got added correctly
 			revalidate();
@@ -491,74 +345,18 @@ public class Game extends JPanel implements ActionListener, MouseListener {
 			disabledCellCounter_++;
 			int cellX = c.x_;
 			int cellY = c.y_;
-			if(c.x_ == 0 && c.y_ == 0) //top left corner case
-			{
-				buttonClick(cellArray[cellX][cellY+1], cellArray, iconArray);
-				buttonClick(cellArray[cellX+1][cellY+1], cellArray, iconArray);
-				buttonClick(cellArray[cellX+1][cellY], cellArray, iconArray);
-			}
-			else if(cellX == 0 && cellY == cols_-1) //top right corner case
-			{
-				buttonClick(cellArray[cellX][cellY-1], cellArray, iconArray);
-				buttonClick(cellArray[cellX+1][cellY-1], cellArray, iconArray);
-				buttonClick(cellArray[cellX+1][cellY], cellArray, iconArray);
-			}
-			else if(cellX == rows_-1 && cellY == 0) //bottom left corner case
-			{
-				buttonClick(cellArray[cellX][cellY+1], cellArray, iconArray);
-				buttonClick(cellArray[cellX-1][cellY+1], cellArray, iconArray);
-				buttonClick(cellArray[cellX-1][cellY], cellArray, iconArray);
-			}
-			else if(cellX == rows_-1 && cellY == cols_-1) //bottom right corner case
-			{
-				buttonClick(cellArray[cellX][cellY-1], cellArray, iconArray);
-				buttonClick(cellArray[cellX-1][cellY-1], cellArray, iconArray);
-				buttonClick(cellArray[cellX-1][cellY], cellArray, iconArray);
-			}
-			else if(cellX == 0) //cell pressed is on top row
-			{
-				buttonClick(cellArray[cellX][cellY-1], cellArray, iconArray);
-				buttonClick(cellArray[cellX+1][cellY-1], cellArray, iconArray);
-				buttonClick(cellArray[cellX+1][cellY], cellArray, iconArray);
-				buttonClick(cellArray[cellX+1][cellY+1], cellArray, iconArray);
-				buttonClick(cellArray[cellX][cellY+1], cellArray, iconArray);
-			}
-			else if(cellX == rows_-1) //cell pressed is on bottom row
-			{
-				buttonClick(cellArray[cellX][cellY-1], cellArray, iconArray);
-				buttonClick(cellArray[cellX-1][cellY-1], cellArray, iconArray);
-				buttonClick(cellArray[cellX-1][cellY], cellArray, iconArray);
-				buttonClick(cellArray[cellX-1][cellY+1], cellArray, iconArray);
-				buttonClick(cellArray[cellX][cellY+1], cellArray, iconArray);
-			}
-			else if(cellY == 0) //cell pressed is on leftmost column
-			{
+			if(cellX == 0 || cellY == 0 || cellX == rows_+1 || cellY == rows_+1) { return; }
+
 				buttonClick(cellArray[cellX-1][cellY],cellArray,iconArray);
-				buttonClick(cellArray[cellX-1][cellY+1],cellArray,iconArray);
-				buttonClick(cellArray[cellX][cellY+1],cellArray,iconArray);
-				buttonClick(cellArray[cellX+1][cellY+1],cellArray,iconArray);
-				buttonClick(cellArray[cellX+1][cellY],cellArray,iconArray);
-			}
-			else if(cellY == cols_-1) //cell pressed is on rightmost column
-			{
-				buttonClick(cellArray[cellX-1][cellY],cellArray,iconArray);
-				buttonClick(cellArray[cellX-1][cellY-1],cellArray,iconArray);
-				buttonClick(cellArray[cellX][cellY-1],cellArray,iconArray);
-				buttonClick(cellArray[cellX+1][cellY-1],cellArray,iconArray);
-				buttonClick(cellArray[cellX+1][cellY],cellArray,iconArray);
-			}
-			else //this else case we do not have to worry about bounds issues
-			{
-				buttonClick(cellArray[cellX-1][cellY],cellArray,iconArray);
-				buttonClick(cellArray[cellX-1][cellY-1],cellArray,iconArray);
-				buttonClick(cellArray[cellX][cellY-1],cellArray,iconArray);
-				buttonClick(cellArray[cellX+1][cellY-1],cellArray,iconArray);
-				buttonClick(cellArray[cellX+1][cellY],cellArray,iconArray);
+				buttonClick(cellArray[cellX-1][cellY-1],cellArray,iconArray); //right now it calls out of bounds on cell array
+				buttonClick(cellArray[cellX][cellY-1],cellArray,iconArray); //since cell array is shifted for border but it still checks
+				buttonClick(cellArray[cellX+1][cellY-1],cellArray,iconArray); //make cell array account for border too
+				buttonClick(cellArray[cellX+1][cellY],cellArray,iconArray); //just dont give it any panels to add to
 				buttonClick(cellArray[cellX-1][cellY+1],cellArray,iconArray);
 				buttonClick(cellArray[cellX][cellY+1],cellArray,iconArray);
 				buttonClick(cellArray[cellX+1][cellY+1],cellArray,iconArray);
 				
-			}
+			//}
 		}
 	}
 	
